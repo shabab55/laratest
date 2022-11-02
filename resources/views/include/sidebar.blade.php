8ee767b91c16a84bac5e82644d447607ba2fc5a8
shabab3092 <112648854+shabab3092@users.noreply.github.com>
2022-11-02 14:37:09 +0600

    file updated and some unused file deleted

---
+++ resources/views/include/sidebar.blade.php
@@ -0,0 +1,25 @@
+<aside id="colorlib-aside" role="complementary" class="js-fullheight text-center">
+    <h1 id="colorlib-logo"><a href="index.html"><span class="img" style="background-image: url(images/author.jpg);"></span>Louie Smith</a></h1>
+    <nav id="colorlib-main-menu" role="navigation">
+        <ul>
+            <li class="colorlib-active"><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('collection') }}">Collection</a></li>
+            <li><a href="{{ route('about') }}">About Me</a></li>
+            <li><a href="{{ route('services') }}">My Services</a></li>
+            <li><a href="{{ route('blog') }}">Blog</a></li>
+            <li><a href="{{ route('contact') }}">Contact</a></li>
+        </ul>
+    </nav>
+
+    <div class="colorlib-footer">
+        <h3>Newsletter</h3>
+        <div class="d-flex justify-content-center">
+            <form action="#" class="colorlib-subscribe-form">
+    <div class="form-group d-flex">
+        <div class="icon"><span class="icon-paper-plane"></span></div>
+      <input type="text" class="form-control" placeholder="Enter Email Address">
+    </div>
+  </form>
+        </div>
+    </div>
+</aside>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Louie - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Herr+Von+Muellerhoff" rel="stylesheet">
+
+    @include('include.style')
+
+  </head>
+  <body>
+
+	<div id="colorlib-page">
+		<a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+		@include('include.sidebar')
+        <!-- END COLORLIB-ASIDE -->
+		<div id="colorlib-main">
+
+            @yield('content')
+
+            <!-- Footer Start -->
+        @include('include.footer')
+        <!-- Footer End -->
+		</div><!-- END COLORLIB-MAIN -->
+	</div><!-- END COLORLIB-PAGE -->
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+    @include('include.scripts')
+
+  </body>
+</html>
